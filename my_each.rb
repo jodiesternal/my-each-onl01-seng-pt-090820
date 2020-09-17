9e7213bbcc4b5cdc1432afcf74ab9be1e8401a09
global-listener-7039 <jodiesternal@yahoo.com>
2020-09-17 18:40:59 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -8,14 +8,12 @@ def my_each(array)
 end
 
 
-def hello(array)
-  i = 0
-  collection = []
-  while i < array.length
-    collection << yield(array[i])
-    i += 1
-  end
-  collection
-end
-
-# hello(list) {|i| "Hello #{i} what's up?"}
+# def hello(array)
+#   i = 0
+#   collection = []
+#   while i < array.length
+#     collection << yield(array[i])
+#     i += 1
+#   end
+#   collection
+# end
